df5878a83d2ce023d3298d33c7afdd3cdc8e240d
Ahmedelabyd2312 <119807950+Ahmedelabyd2312@users.noreply.github.com>
2024-12-10 03:36:56 +0200

    back login

--- Tolip/Form2.Designer.cs
+++ Tolip/Form2.Designer.cs
@@ -30,9 +30,10 @@ namespace Tolip
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.textBoxUsername = new System.Windows.Forms.TextBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label3 = new System.Windows.Forms.Label();
@@ -45,6 +46,7 @@ namespace Tolip
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -57,8 +59,8 @@ namespace Tolip
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.textBoxPassword);
+            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.textBoxUsername);
             this.groupBox1.Controls.Add(this.linkLabel1);
             this.groupBox1.Controls.Add(this.label3);
@@ -66,16 +68,23 @@ namespace Tolip
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.pictureBox3);
             this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.Sienna;
-            this.groupBox1.Location = new System.Drawing.Point(52, 228);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(42, 182);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(742, 451);
+            this.groupBox1.Size = new System.Drawing.Size(1512, 361);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Please Login First";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(128, 146);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(307, 26);
+            this.textBoxPassword.TabIndex = 5;
             // 
             // button1
             // 
@@ -84,25 +93,21 @@ namespace Tolip
             this.button1.FlatAppearance.BorderSize = 0;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(160, 356);
+            this.button1.Location = new System.Drawing.Point(128, 285);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(365, 48);
+            this.button1.Size = new System.Drawing.Size(292, 38);
             this.button1.TabIndex = 6;
             this.button1.Text = "Log In";
             this.button1.UseVisualStyleBackColor = false;
-            // 
-            // textBoxPassword
-            // 
-            this.textBoxPassword.Location = new System.Drawing.Point(160, 182);
-            this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(381, 30);
-            this.textBoxPassword.TabIndex = 5;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBoxUsername
             // 
-            this.textBoxUsername.Location = new System.Drawing.Point(160, 92);
+            this.textBoxUsername.Location = new System.Drawing.Point(128, 74);
+            this.textBoxUsername.Margin = new System.Windows.Forms.Padding(2);
             this.textBoxUsername.Name = "textBoxUsername";
-            this.textBoxUsername.Size = new System.Drawing.Size(411, 30);
+            this.textBoxUsername.Size = new System.Drawing.Size(330, 26);
             this.textBoxUsername.TabIndex = 4;
             // 
             // linkLabel1
@@ -110,9 +115,10 @@ namespace Tolip
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.linkLabel1.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(402, 302);
+            this.linkLabel1.Location = new System.Drawing.Point(322, 242);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(101, 27);
+            this.linkLabel1.Size = new System.Drawing.Size(82, 22);
             this.linkLabel1.TabIndex = 3;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Register";
@@ -121,9 +127,10 @@ namespace Tolip
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(238, 306);
+            this.label3.Location = new System.Drawing.Point(190, 245);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(170, 23);
+            this.label3.Size = new System.Drawing.Size(133, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "Forgot Password?";
             // 
@@ -131,9 +138,10 @@ namespace Tolip
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(157, 151);
+            this.label2.Location = new System.Drawing.Point(126, 121);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 23);
+            this.label2.Size = new System.Drawing.Size(79, 18);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password:";
             // 
@@ -141,62 +149,74 @@ namespace Tolip
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(157, 61);
+            this.label1.Location = new System.Drawing.Point(126, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 23);
+            this.label1.Size = new System.Drawing.Size(86, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username:";
             // 
             // pictureBox3
             // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.WaitCursor;
             this.pictureBox3.Image = global::Tolip.Properties.Resources._32;
-            this.pictureBox3.Location = new System.Drawing.Point(547, 182);
+            this.pictureBox3.Location = new System.Drawing.Point(439, 146);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(24, 30);
+            this.pictureBox3.Size = new System.Drawing.Size(19, 24);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 8;
             this.pictureBox3.TabStop = false;
+            this.pictureBox3.UseWaitCursor = true;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBox3.MouseHover += new System.EventHandler(this.pictureBox3_MouseHover);
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
             this.pictureBox2.Image = global::Tolip.Properties.Resources._22;
-            this.pictureBox2.Location = new System.Drawing.Point(547, 182);
+            this.pictureBox2.Location = new System.Drawing.Point(439, 146);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(24, 30);
+            this.pictureBox2.Size = new System.Drawing.Size(19, 24);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.UseWaitCursor = true;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.MouseHover += new System.EventHandler(this.pictureBox2_MouseHover);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Sienna;
-            this.label4.Location = new System.Drawing.Point(1564, 797);
+            this.label4.Location = new System.Drawing.Point(1251, 638);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(182, 40);
+            this.label4.Size = new System.Drawing.Size(151, 33);
             this.label4.TabIndex = 3;
             this.label4.Text = "Tolip Hotel";
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.LightGray;
-
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Location = new System.Drawing.Point(837, 155);
-
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(10, 1055);
+            this.panel1.Size = new System.Drawing.Size(8, 735);
             this.panel1.TabIndex = 4;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = global::Tolip.Properties.Resources._11;
-            this.pictureBox1.Location = new System.Drawing.Point(913, 252);
+            this.pictureBox1.Location = new System.Drawing.Point(1648, 202);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(558, 380);
+            this.pictureBox1.Size = new System.Drawing.Size(446, 304);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
@@ -205,29 +225,25 @@ namespace Tolip
             // 
             this.panel2.Controls.Add(this.label5);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-
-            this.panel2.Location = new System.Drawing.Point(10, 975);
+            this.panel2.Location = new System.Drawing.Point(8, 671);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1914, 80);
-
-            this.panel2.Location = new System.Drawing.Point(0, 812);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1738, 80);
-
+            this.panel2.Size = new System.Drawing.Size(1394, 64);
             this.panel2.TabIndex = 5;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 55);
+            this.label5.Location = new System.Drawing.Point(10, 44);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(264, 21);
+            this.label5.Size = new System.Drawing.Size(205, 17);
             this.label5.TabIndex = 0;
             this.label5.Text = "CopyRight@.All Right Reserved";
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.AutoSize = true;
@@ -235,14 +251,13 @@ namespace Tolip
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::Tolip.Properties.Resources._61;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1738, 892);
+            this.ClientSize = new System.Drawing.Size(1096, 599);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
@@ -275,5 +290,6 @@ namespace Tolip
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label5;
+        private ToolTip toolTip1;
     }
 }
--- Tolip/Form2.cs
+++ Tolip/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Tolip
 {
@@ -22,6 +23,55 @@ namespace Tolip
             Register register = new Register();
             register.Show();
             this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool check = db.IsValidNamePass(textBoxUsername.Text.Trim(), textBoxPassword.Text.Trim());
+            if (textBoxUsername.Text.Trim() == string.Empty || textBoxPassword.Text.Trim() == string.Empty)
+                MessageBox.Show("Please fill out all field.", "Required field", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+            {
+                if (check)
+                {
+                    Home fd = new Home();
+                    fd.Username = textBoxUsername.Text;
+                    textBoxUsername.Clear();
+                    textBoxPassword.Clear();
+                    fd.Show();
+                }
+                else
+                    MessageBox.Show("Invalid Username or Passwod.", "Username or Password", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            pictureBox3.Hide();
+            textBoxPassword.UseSystemPasswordChar = true;
+            pictureBox2.Show();
+        }
+       
+
+        
+
+        private void pictureBox3_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(pictureBox3, "Hide Password");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Hide();
+            textBoxPassword.UseSystemPasswordChar = false;
+            pictureBox3.Show();
+        }
+
+        private void pictureBox2_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(pictureBox2, "Show Password");
         }
     }
 }
